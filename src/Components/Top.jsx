a927447f0cf99fdaf8c71879fa182204cc3481e7
Rengoku03 <c8709040124@gmail.com>
2024-08-30 18:45:57 +0530

    image load error

--- src/Components/Top.jsx
+++ src/Components/Top.jsx
@@ -10,7 +10,7 @@ const Top = () => {
           {/* <h1 className="text-white px-3 py-1 rounded-3xl font-bold text-2xl ml-4 md:text-5xl bg-[#FC1503]">
             ViewTube
           </h1> */}
-          <img src="./viewtubelogo.png" alt="viewtube-logo" className="h-10 w-24 xs:w-auto xs:h-12 md:h-16 ml-4" />
+          <img src='/./viewtubelogo.png' alt="viewtube-logo" className="h-10 w-24 xs:w-auto xs:h-12 md:h-16 ml-4" />
         </div>
       </Link>
       <SearchBar />
